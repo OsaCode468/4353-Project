c4cfd37021ec56016acfb5e62c57c27ccb2777e6
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-02-12 23:04:50 -0600

    added links to navbar

--- frontend/my-app/src/app/components/Navbar.js
+++ frontend/my-app/src/app/components/Navbar.js
@@ -2,9 +2,13 @@ const Navbar = () =>{
     return (
         <div className="flex items-center justify-center bg-lime-500 h-48">
           <ul className = "flex gap-5">
-            <li>Home</li>
+            <a href = "/">
+              <li>Home</li>
+            </a>
             <h1>Registration</h1>
-            <li>Login</li>
+            <a href = "login">
+              <li>Login</li>
+            </a>
             <h1>Client Profile Management</h1>
             <h1>Fuel Quote Form</h1>
             <h1>Fuel Quote History</h1>
