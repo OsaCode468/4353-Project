da2b93897f1a9780a3c007142303cf9f3d2c94d1
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-02-12 21:25:09 -0600

    added navbar

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -2,6 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hi</h1>
+    <div className="flex items-center justify-center bg-lime-500 h-48">
+      <ul className = "flex gap-5">
+        <li>Home</li>
+        <li>Login</li>
+        <h1>Client Profile Management</h1>
+        <h1>Fuel Quote Form</h1>
+        <h1>Fuel Quote History</h1>
+      </ul>
+    </div>
   );
 }
