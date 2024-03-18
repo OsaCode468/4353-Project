dbde9459fe7247e0b305db56e4c8105e64227579
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-03-18 05:18:20 -0500

    set up routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const loginRoutes = require("./routes/login")
+const clientModuleRoutes = require("./routes/clientModule")
+const pricingRoutes = require("./routes/pricingModule")
+const fuelRoutes = require("./routes/fuelQuoteModule")
+
 app.get("/", (req, res) => {
     res.send("hi")
 })
+
+app.use("/api/login", loginRoutes)
+app.use("/api/clientmodule", clientModuleRoutes)
+app.use("/api/pricing", pricingRoutes)
+app.use("/api/fuelQuote", fuelRoutes)
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
