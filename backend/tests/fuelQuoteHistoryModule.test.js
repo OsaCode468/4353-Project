eef65b4cff753c3ea86549842c226ae7fd67199d
Melvin Syriac <melvinsyriac@gmail.com>
2024-03-21 23:41:49 -0500

    Small Changes

--- backend/tests/fuelQuoteHistoryModule.test.js
+++ backend/tests/fuelQuoteHistoryModule.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const express = require("express");
-const router = require("../routes/fuelQuoteHistoryModule"); // Replace with the actual path to your router file
+const router = require("../routes/fuelQuoteHistoryModule");
 
 const app = express();
 app.use("/", router);
