f84f877d96627d68bc9cbf3d7ed5c887fa8eb000
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-04-09 02:22:17 -0500

    reconfigured login route to work with db

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -10,21 +10,30 @@ const dummypass = [];
 
 
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
 
     const { username, password } = req.body;
+    const client = await pool.connect()
 
     if (!username || !password) {
         return res.status(400).json({ "err": "all fields must be filled" });
     }
-    if (!dummyuser.includes(username)) {
-        return res.status(400).json({ "err": "username does not exist" });
+    const userExists = await client.query('SELECT COUNT(*) FROM users WHERE username = $1', [username])
+    if (parseInt(userExists.rows[0]?.count) <= 0){
+        throw Error("Username does not exist")
     }
-    if (!dummypass.includes(password)) {
-        return res.status(400).json({ "err": "password does not exist" });
+
+    const ress = await client.query('SELECT password FROM users WHERE username = $1', [username])
+    dbPassword = ress.rows[0]?.password
+    const match = await bcrypt.compare(password, dbPassword)
+    if (!match) {
+        throw Error("Incorrect password")
     }
+    const _id = await client.query("SELECT id FROM users WHERE username = $1", [username])
+    const id = _id.rows[0]?.id
+    const token = jwt.sign({id}, process.env.SECRET, {expiresIn: '3d'})
+    res.status(200).json({username, token})
     // console.log(`success ${username} and ${password}`);
-    res.status(200).json({ "jsonwebtoken": 5667 });
 });
 
 router.post("/", async (req, res) => {
@@ -38,7 +47,7 @@ router.post("/", async (req, res) => {
     if (parseInt(userExists.rows[0]?.count) > 0){
         throw Error("Username already in use")
     } 
-
+    
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
@@ -47,19 +56,20 @@ router.post("/", async (req, res) => {
         console.log("User inserted successfully:", statement);
         const _id = await client.query("SELECT id FROM users WHERE username = $1", [username])
         const id = _id.rows[0]?.id
-        const token = jwt.sign({id}, proces.env.SECRET, {expiresIn: '3d'})
+        const token = jwt.sign({id}, process.env.SECRET, {expiresIn: '3d'})
+        res.status(200).json({username, token})
     } catch (error) {
         console.error("Error inserting user:", error);
         return res.status(500).json({ "err": "Internal server error" });
     } finally {
         client.release()
     }
-    dummyuser.push(username);
-    dummypass.push(hash); // Store hashed password
+    // dummyuser.push(username);
+    // dummypass.push(hash); // Store hashed password
 
-    // console.log(dummyuser);
+    // // console.log(dummyuser);
 
-    return res.status(200).json({"statement": statement});
+    // return res.status(200).json({"statement": statement});
 });
 
 module.exports = router;
