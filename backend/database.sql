4ff8ff60e914dd5aa287550ae456a0a2cb5e1dd0
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-04-12 18:37:56 -0500

    changed gitignore and database.sql:

--- backend/database.sql
+++ backend/database.sql
@@ -5,3 +5,23 @@ CREATE TABLE IF NOT EXISTS users (
         username VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL
       )
+CREATE TABLE IF NOT EXISTS fuelquotes (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    gallons_requested INT NOT NULL,
+    delivery_address TEXT NOT NULL,
+    delivery_date DATE NOT NULL,
+    price_per_gallon DECIMAL NOT NULL,
+    total_amount_due DECIMAL NOT NULL
+);
+
+INSERT INTO users (id, username, password)
+VALUES
+    (1, 'username', 'password');
+    (2, 'usernames', 'passwords');
+
+INSERT INTO fuelquotes (user_id, gallons_requested, delivery_address, delivery_date, price_per_gallon, total_amount_due)
+VALUES
+    (1, 550, '7404 Marlborough Rd. New Kensington, PA 15068', '2024-05-20', 3.276, 1801.80),
+    (1, 350, '7404 Marlborough Rd. New Kensington, PA 15068', '2024-04-22', 3.279, 1147.65),
+    (1, 600, '7404 Marlborough Rd. New Kensington, PA 15068', '2024-03-15', 3.225, 1935.00);
