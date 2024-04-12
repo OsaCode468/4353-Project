1708706ff74336556e16b7cdd5c47681433143f6
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-04-12 17:11:55 -0500

    unit tests working

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -20,14 +20,14 @@ router.post("/login", async (req, res) => {
     }
     const userExists = await client.query('SELECT COUNT(*) FROM users WHERE username = $1', [username])
     if (parseInt(userExists.rows[0]?.count) <= 0){
-        throw Error("Username does not exist")
+        return res.status(400).json({"err":"Username does not exist"})
     }
 
     const ress = await client.query('SELECT password FROM users WHERE username = $1', [username])
-    dbPassword = ress.rows[0]?.password
+    const dbPassword = ress.rows[0]?.password
     const match = await bcrypt.compare(password, dbPassword)
     if (!match) {
-        throw Error("Incorrect password")
+        return res.status(400).json({"err":"Incorrect password"})
     }
     const _id = await client.query("SELECT id FROM users WHERE username = $1", [username])
     const id = _id.rows[0]?.id
@@ -38,29 +38,26 @@ router.post("/login", async (req, res) => {
 
 router.post("/", async (req, res) => {
     const { username, password } = req.body;
-
     if (!username || !password) {
         return res.status(400).json({ "err": "all fields must be filled" });
     }
     const client = await pool.connect()
     const userExists = await client.query('SELECT COUNT(*) FROM users WHERE username = $1', [username])
     if (parseInt(userExists.rows[0]?.count) > 0){
-        throw Error("Username already in use")
+        return res.status(400).json({"err":"Username already in use"})
     } 
     
-
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     try {
-        statement = await client.query(`INSERT INTO users (username, password) VALUES($1, $2)`, [username, hash]);
-        console.log("User inserted successfully:", statement);
+        statement = await client.query(`INSERT INTO users (username, password) VALUES($1, $2)`, [username, password]); //replace with hash later
         const _id = await client.query("SELECT id FROM users WHERE username = $1", [username])
         const id = _id.rows[0]?.id
-        const token = jwt.sign({id}, process.env.SECRET, {expiresIn: '3d'})
+        const token = jwt.sign({id}, "YGBhNeFUN9iGuk8Hhed4qtx1thdxlOSX", {expiresIn: '3d'})
         res.status(200).json({username, token})
     } catch (error) {
         console.error("Error inserting user:", error);
-        return res.status(500).json({ "err": "Internal server error" });
+        return res.status(500).json({ "err": error });
     } finally {
         client.release()
     }
